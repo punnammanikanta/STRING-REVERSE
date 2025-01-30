9025b722fa0245e2fe4d1a9afa2e1a311545e49d
punnammanikanta <PUNNAMMANIKANTA52@GMAIL.COM>
2025-01-30 12:18:23 +0530

    Add files via upload

---
+++ Reversestring.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Reversestring {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String inputString = scanner.nextLine();
+        String reversedString = reverseString(inputString);
+        System.out.println("Reversed string: " + reversedString);
+        scanner.close();
+    }
+     public static String reverseString(String str) {
+     StringBuilder reversed = new StringBuilder();
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed.append(str.charAt(i));
+        }
+             return reversed.toString();
+    }
+}
+    
+
